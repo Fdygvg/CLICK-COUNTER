48d5a8fe826152a5c69c47048a3da514a54114bf
Esowa Chukwuyem <esowaknowledge@email.com>
2025-10-28 02:02:48 -1100

    Completed Button Counter

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const colorArr = [
-  "red",
-  "blue",
-  "orange",
-  "green",
-  "yellow",
-  "purple",
-  "violet",
-  "tomato",
+  "red", "blue", "orange", "green", "yellow", "purple", "violet", "tomato"
 ];
 const button = document.querySelector(".button");
 const margin = document.querySelector(".btn-div");
+let num = document.querySelector(".num");
 let i = 0;
 let j = 0;
+let k = 1;
+
 button.addEventListener("click", function changeColor() {
-  let marginNum = Math.floor(Math.random() * (500 - 100 + 1) + 100);
-  console.log(marginNum);
-  
-  margin.style.margin = marginNum + "px";
+  let marginNum = Math.floor(Math.random() * (1000 - (0) + 1) + 100);
+  margin.style.marginLeft = marginNum + "px";
 
+
+  num.style.color = colorArr[j];
   button.style.backgroundColor = colorArr[i];
-  i++;
+  i++; j++;
+  if (i === colorArr.length) i = 0;
+  if (j === colorArr.length) j = 0;
 
-  if (i === colorArr.length) {
-    i = 0;
+  // Increment logic
+  if (k > 500) {
+    k += 10;
+  } else if (k > 200) {
+    k += 5;
+  } else if (k > 100) {
+    k += 3;
+  } else if (k > 50) {
+    k += 2;
+  } else {
+    k++;
   }
+
+  num.innerText = k;
 });
